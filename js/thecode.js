0aef8dcd0f325df0a0a37f11ed58407657527aed
tslnc04 <timothy.laskoski@gmail.com>
2016-03-05 09:55:41 -0500

    save

--- js/thecode.js
+++ js/thecode.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    $('.fa-apple').hover(function() {
-        $(this).addClass('fa-spin');
+    $("#spinHover").hover(function() {
+        $(this).toggleClass('fa-spin');
     });
 });
